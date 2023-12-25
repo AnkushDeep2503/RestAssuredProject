96d6600ff4aa124c63ac2291ae0a5beff48292f7
AnkushDeep2503 <nkusme@gmail.com>
2023-12-24 22:07:05 -0500

    new changes

---
+++ src/main/java/Testing/staticJsonFileAsPathInBody.java
@@ -0,0 +1,46 @@
+package Testing;
+
+import Files.payload;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class staticJsonFileAsPathInBody {
+
+        public static void main(String[] args) throws IOException {
+
+            String JsonFilePath = "restAssured.json";
+            Path path = Paths.get(JsonFilePath);
+            String jsonContent = new String (Files.readAllBytes(path));
+            System.out.println(jsonContent+"<<<<This one====>>>>");
+            RestAssured.baseURI = "https://rahulshettyacademy.com";
+
+            String response = given().log().all().queryParam("key", "qaclick123")
+                    .header("Content-Type", "application/json")
+                    .body(jsonContent)
+
+                    .when().post("/maps/api/place/add/json")
+
+                    .then().assertThat().statusCode(200)
+                    .body("scope", equalTo("APP"))
+                    .header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
+
+            System.out.println(response);
+
+            JsonPath js = new JsonPath(response); // for parsing Json
+            String placeID = js.getString("place_id"); //we have put js.getString with argument path inside a variable String placeID
+            System.out.println("The retrieved place id is: --> " +
+                    ""+ placeID);
+        }
+    }
+
